a7ed6951301ec65b15e2d6c11c5315f8983c1216
JerylDEv <jeryldestopace@gmail.com>
2018-11-22 18:51:32 +0800

    Adding and removing babel-eslint

--- js/chapter3.js
+++ js/chapter3.js
@@ -228,3 +228,75 @@ function raise(base, exponent) {
 
 const raiseNumber = raise(2, 10);
 console.log(raiseNumber);
+
+// The function calls itself multiple times with ever smaller exponents to achieve the repeated multiplication.
+// But this implementation has one problem: in typical JavaScript implementations,
+// it’s about three times slower than the looping version.
+// Running through a simple loop is generally cheaper than calling a function multiple times.
+// Therefore, always start by writing something that’s correct and easy to understand.
+// If you’re worried that it’s too slow—which it usually isn’t since most code simply isn’t executed often enough
+// to take any significant amount of time—you can measure afterward and improve it if necessary. (Page 49).
+
+// Consider this puzzle: by starting from the number 1 and repeatedly either adding 5 or multiplying by 3,
+// an infinite set of numbers can be produced. How would you write a function that, given a number,
+// tries to find a sequence of such additions and multiplications that produces that number? (Page 50).
+
+/**
+ * findSolution function
+ * @param {number} target
+ * @return {number}
+ */
+function findSolution(target) {
+	/**
+	 * find function - recursive
+	 * @param {number} current
+	 * @param {number} history
+	 * @return {string}
+	 */
+	function find(current, history) {
+		if (current == target) {
+			return history;
+		} else if (current > target) {
+			return null;
+		} else {
+			return find(current + 5, `${history} + 5`) || find(current * 3, `${history} * 3`);
+		}
+	}
+	return find(1, '1');
+}
+
+const puzzle1 = findSolution(18);
+console.log(puzzle1); // 1 + 5 * 3
+
+const puzzle2 = findSolution(15);
+console.log(puzzle2); // null
+
+// The inner function find does the actual recursing.
+// It takes two arguments: the current number and a string that records how we reached this number.
+// If it finds a solution, it returns a string that shows how to get to the target.
+// If no solution can be found starting from this number, it returns null.
+// To do this, the function performs one of three actions. If the current number is the target number,
+// the current history is a way to reach that target, so it is returned.
+// If the current number is greater than the target, there’s no sense in further exploring this branch
+// because both adding and multiplying will only make the number bigger, so it returns null.
+// Finally, if we’re still below the target number, the function tries both possible paths that start
+// from the current number by calling itself twice, once for addition and once for multiplication.
+// If the first call returns something that is not null, it is returned.
+// Otherwise, the second call is returned, regardless of whether it produces a string or null. (Page 51).
+
+// Growing Functions
+
+// There are 2  or more natural ways for functions to be created in programs
+// 1. The first is that you find yourself writing similar code multiple times.
+// You’d prefer not to do that. Having more code means more space for mistakes to hide and more material
+// to read for people trying to understand the program. So you take the repeated functionality,
+// find a good name for it, and put it into a function.
+// 2. The second way is that you find you need some functionality that you haven’t written yet and that
+// sounds like it deserves its own function. You’ll start by naming the function, and then you’ll write its body.
+// You might even start writing code that uses the function before you actually define the function itself.
+// How difficult it is to find a good name for a function is a good indication of how clear a concept it is
+// that you’re trying to wrap. (Page 51 to 52).
+
+// Functions and their Side Effects
+
+//
